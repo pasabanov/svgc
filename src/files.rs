db46ca258837d77c2f890fa46d81000b4855f8b3
pasabanov <pasabanov@murena.io>
2024-08-24 06:41:41 +0300

    Reworked backups; made output more verbose; added -q option

--- src/files.rs
+++ src/files.rs
@@ -14,25 +14,192 @@
 //! You should have received a copy of the GNU General Public License
 //! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::fs;
-use std::io;
-use std::path::PathBuf;
+use std::{env, fs, io};
+use std::path::{PathBuf};
+use chrono::Local;
 
-pub(crate) fn find_svg_files(vec_to_append: &mut Vec<PathBuf>, path: &PathBuf, recursive: bool) -> io::Result<()> {
-	if path.is_file() {
-		if path.extension().and_then(|e| e.to_str()) == Some("svg") {
-			vec_to_append.push(path.clone());
+fn unique_timestamp() -> String {
+	Local::now().format("%Y-%m-%d_%H-%M-%S_%f").to_string()
+}
+
+fn generate_temp_dir_name() -> String {
+	format!("svgc_temp_files_{}", unique_timestamp())
+}
+
+fn try_create_temp_dir(path: &PathBuf, name: &str) -> io::Result<PathBuf> {
+	let temp_dir = path.join(name);
+	fs::create_dir_all(&temp_dir)?;
+	Ok(temp_dir)
+}
+
+pub fn create_temp_dir() -> Option<PathBuf> {
+	let temp_dir_name = generate_temp_dir_name();
+
+	// Trying to generate temporary directory in some of these directories
+	let directories = [
+		|| { // current directory, checking that is not temporary
+			let current = fs::canonicalize(env::current_dir().ok()?).ok()?;
+			let temp = fs::canonicalize(env::temp_dir()).ok()?;
+			if current != temp { Some(current) } else { None }
+		},
+		|| dirs::home_dir(),
+		|| directories::ProjectDirs::from("org", "pasabanov", "svgc").map(|dirs| dirs.data_dir().to_path_buf()),
+	];
+
+	for (i, get_dir) in directories.iter().enumerate() {
+		if let Some(dir) = get_dir() {
+			if let Ok(temp_dir) = try_create_temp_dir(&dir, &temp_dir_name) {
+				return Some(temp_dir);
+			}
+			eprintln!(
+				"Couldn't create a temporary directory in {}.{}",
+				dir.display(),
+				if i < directories.len() - 1 { " Trying next." } else { "" }
+			);
 		}
-		return Ok(());
 	}
-	for entry in fs::read_dir(path)? {
+
+	None
+}
+
+pub struct TempBackupStorage {
+	paths: Vec<(PathBuf, PathBuf)>,
+	temp_dir: PathBuf,
+	auto_cleanup: bool,
+}
+
+#[allow(dead_code)]
+impl TempBackupStorage {
+
+	pub fn new(original_paths: &[PathBuf]) -> io::Result<Self> {
+
+		let temp_dir = create_temp_dir()
+			.ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Could not create temporary directory"))?;
+
+		let mut paths = Vec::new();
+
+		for path in original_paths {
+			let temp_path = temp_dir.join(
+				format!(
+					"{}_{}.{}",
+					path.file_stem()
+						.ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, format!("Could not get file {} name", path.display())))?
+						.to_string_lossy(),
+					unique_timestamp(),
+					path.extension()
+						.ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, format!("Could not get file {} extension", path.display())))?
+						.to_string_lossy(),
+				)
+			);
+			if path.is_file() {
+				fs::copy(path, &temp_path)?;
+			} else if path.is_dir() {
+				copy_dir(path, &temp_path)?;
+			}
+			paths.push((path.clone(), temp_path));
+		}
+
+		Ok(Self { paths, temp_dir, auto_cleanup: true, })
+	}
+
+	pub fn copy_back(&self) -> io::Result<()> {
+		for (orig_path, temp_bak_path) in self.paths.iter() {
+			if let Some(parent) = temp_bak_path.parent() {
+				fs::create_dir_all(parent)?;
+			} else {
+				eprintln!("Warning! Could not get parent directory of file {}", temp_bak_path.display());
+			}
+			if temp_bak_path.is_file() {
+				fs::copy(temp_bak_path, orig_path)?;
+			} else if temp_bak_path.is_dir() {
+				copy_dir(temp_bak_path, orig_path)?;
+			}
+		}
+		Ok(())
+	}
+
+	fn orig_paths_iter(&self) -> impl Iterator<Item = &PathBuf> {
+		self.paths.iter().map(|(orig, _)| orig)
+	}
+
+	fn temp_paths_iter(&self) -> impl Iterator<Item = &PathBuf> {
+		self.paths.iter().map(|(_, temp)| temp)
+	}
+
+	pub fn temp_dir(&self) -> &PathBuf {
+		&self.temp_dir
+	}
+
+	pub fn is_auto_cleanup(&self) -> bool {
+		self.auto_cleanup
+	}
+
+	pub fn disable_auto_cleanup(&mut self) {
+		self.auto_cleanup = false
+	}
+
+	pub fn enable_auto_cleanup(&mut self) {
+		self.auto_cleanup = true
+	}
+
+	pub fn set_auto_cleanup(&mut self, auto_cleanup: bool) {
+		self.auto_cleanup = auto_cleanup
+	}
+
+	pub fn cleanup(&mut self) -> io::Result<()> {
+		fs::remove_dir_all(&self.temp_dir)
+	}
+}
+
+impl Drop for TempBackupStorage {
+	fn drop(&mut self) {
+		if self.auto_cleanup {
+			if let Err(e) = self.cleanup() {
+				eprintln!("Failed to delete temporary directory {}: {}", self.temp_dir.display(), e);
+			}
+		}
+	}
+}
+
+fn copy_dir(src: &PathBuf, dst: &PathBuf) -> io::Result<()> {
+	fs::create_dir_all(&dst)?;
+	for entry in fs::read_dir(src)? {
 		let entry = entry?;
-		let path = entry.path();
-		if path.is_dir() && recursive {
-			find_svg_files(vec_to_append, &path, recursive)?;
-		} else if path.extension().and_then(|e| e.to_str()) == Some("svg") {
-			vec_to_append.push(path.clone());
+		let src_path = entry.path();
+		let dst_path = dst.join(entry.file_name());
+		if src_path.is_file() {
+			fs::copy(src_path, dst_path)?;
+		} else if src_path.is_dir() {
+			copy_dir(&src_path, &dst_path)?;
 		}
 	}
 	Ok(())
+}
+
+pub fn find_svg_files(paths: &[PathBuf], recursive: bool) -> io::Result<Vec<PathBuf>> {
+
+	fn find_svg_files0(vec_to_append: &mut Vec<PathBuf>, path: &PathBuf, recursive: bool) -> io::Result<()> {
+		if path.is_file() {
+			if path.extension().and_then(|e| e.to_str()) == Some("svg") {
+				vec_to_append.push(path.clone());
+			}
+			return Ok(())
+		} else if !path.is_dir() {
+			return Ok(())
+		}
+		for entry in fs::read_dir(path)? {
+			let entry = entry?;
+			let path = entry.path();
+			if path.is_file() || recursive && path.is_dir() {
+				find_svg_files0(vec_to_append, &path, recursive)?;
+			}
+		}
+		Ok(())
+	}
+
+	let mut svg_files = Vec::new();
+	for temp_path in paths {
+		find_svg_files0(&mut svg_files, &temp_path, recursive)?;
+	}
+	Ok(svg_files)
 }
--- src/main.rs
+++ src/main.rs
@@ -14,108 +14,177 @@
 //! You should have received a copy of the GNU General Public License
 //! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::io;
+use std::collections::HashSet;
 use std::fs;
 use std::path::{PathBuf};
+use std::process::ExitCode;
 
-use clap::{Arg, ArgAction::SetTrue, Command};
+use clap::{Arg, ArgAction, ArgAction::SetTrue, Command};
 
 mod default_opt;
+mod files;
 mod svgo;
 mod svgz;
-mod files;
 
-use default_opt::default_optimize;
+use default_opt::default_optimize_files;
+use files::TempBackupStorage;
 use svgo::run_svgo;
 use svgz::compress_to_svgz;
-use files::find_svg_files;
 
-fn main() -> io::Result<()> {
+fn main() -> ExitCode {
 	let matches = Command::new("svgc")
-		.version("0.1.5")
+		.about("Compress SVG files by removing unnecessary whitespace, comments, metadata, and some other redundant data.\n\
+				Optionally, you can use SVGO for additional optimization, and compress the files to .svgz format.\n\
+				\n\
+				This program DOES NOT create backups (outside of the program's lifetime) if it runs successfully, so use it carefully!\n\
+				If an error occurs, backups of the original files will be saved.")
+		.version("0.1.6")
 		.long_version(
-			"0.1.5\n\
+			"0.1.6\n\
 			Copyright (C) 2024 Petr Alexandrovich Sabanov.\n\
 			License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n\
 			This is free software: you are free to change and redistribute it.\n\
 			There is NO WARRANTY, to the extent permitted by law."
 		)
-		.about("Compress SVG files by removing unnecessary whitespace, comments, metadata, and some other data.")
-		.arg(Arg::new("paths").help("List of SVG files or directories or SVG files to compress")
+		.arg(Arg::new("paths").help("List of SVG files or directories containing SVG files to compress")
 			.required(true)
 			.num_args(1..))
 		.arg(Arg::new("recursive")  .short('r').long("recursive")  .help("Recursively process directories")
 			.action(SetTrue))
 		.arg(Arg::new("remove-fill").short('f').long("remove-fill").help("Remove fill=\"...\" attributes")
 			.action(SetTrue))
-		.arg(Arg::new("svgo")       .short('o').long("svgo")       .help("Use SVGO if it exists in the system")
+		.arg(Arg::new("svgo")       .short('o').long("svgo")       .help("Use SVGO if it is installed on the system")
 			.action(SetTrue))
-		.arg(Arg::new("svgz")       .short('z').long("svgz")       .help("Compress to .svgz format with gzip utility after processing")
+		.arg(Arg::new("svgz")       .short('z').long("svgz")       .help("Compress to .svgz format after optimization")
 			.action(SetTrue))
-		.arg(Arg::new("no-default") .short('n').long("no-default") .help("Skip default optimizations")
+		.arg(Arg::new("no-default") .short('n').long("no-default") .help("Do not perform default optimizations")
 			.action(SetTrue))
-		.arg(Arg::new("no-backup")  .short('B').long("no-backup")  .help("Do not create backup (.bak) files")
+		.arg(Arg::new("quiet")      .short('q').long("quiet")      .help("Only output error messages, not regular status messages")
 			.action(SetTrue))
+		.disable_version_flag(true)
+		.arg(Arg::new("version")    .short('v').long("version")    .help("Print version").action(ArgAction::Version))
+		.disable_help_flag(true)
+		.arg(Arg::new("help")       .short('h').long("help")       .help("Print help")   .action(ArgAction::Help))
 		.get_matches();
 
-	let paths: Vec<PathBuf> = matches.get_many::<String>("paths").unwrap().map(PathBuf::from).collect();
+	let paths: Vec<PathBuf> =
+		matches
+			.get_many::<String>("paths")
+			.unwrap()
+			.map(PathBuf::from)
+			.filter_map(|path| {
+				match fs::canonicalize(&path) {
+					Ok(canon) => {
+						if canon.is_dir() || canon.is_file() && canon.extension().unwrap_or_default().eq("svg") {
+							Some(canon)
+						} else {
+							eprintln!("The path {} is not a valid SVG file or directory.", path.display());
+							None
+						}
+					}
+					Err(e) => {
+						eprintln!("The path {} does not exist or cannot be accessed. Error: {}", path.display(), e);
+						None
+					}
+				}
+			})
+			.collect::<HashSet<PathBuf>>()
+			.into_iter()
+			.collect();
 	let recursive = matches.get_flag("recursive");
 	let remove_fill = matches.get_flag("remove-fill");
 	let use_svgo = matches.get_flag("svgo");
 	let compress_svgz = matches.get_flag("svgz");
 	let no_default = matches.get_flag("no-default");
-	let no_backup = matches.get_flag("no-backup");
+	let quiet = matches.get_flag("quiet");
 
-	if paths.is_empty() || no_default && !use_svgo && !compress_svgz {
-		return Ok(())
+	if no_default && !use_svgo && !compress_svgz {
+		if !quiet {
+			println!("No actions specified. Your files were not modified.");
+			println!("Type 'svgc --help' for more information.");
+		}
+		return ExitCode::SUCCESS
 	}
 
 	let svgo_path = if use_svgo {
 		match which::which("svgo") {
 			Ok(path) => Some(path.display().to_string()),
 			Err(_) => {
-				eprintln!("Error: svgo executable not found in the system.");
-				None
+				eprintln!("Error: SVGO is not installed.");
+				if !quiet { println!("Your files weren't modified."); }
+				return ExitCode::FAILURE
 			}
 		}
 	} else {
 		None
 	};
 
-	let mut svg_files = vec!();
-
-	for path in paths {
-		find_svg_files(&mut svg_files, &path, recursive)?
+	if paths.is_empty() {
+		return ExitCode::SUCCESS
 	}
 
-	if !no_backup {
-		for file in &svg_files {
-			let backup_file = file.with_extension(".bak");
-			if backup_file.exists() {
-				return Err(io::Error::new(
-					io::ErrorKind::AlreadyExists,
-					"Backup file already exists"
-				))
-			}
-			fs::copy(file, backup_file)?;
+	let svg_files = match files::find_svg_files(&paths, recursive) {
+		Ok(files) => files,
+		Err(e) => {
+			eprintln!("Error finding svg files: {e}");
+			if !quiet { println!("Your files weren't modified."); }
+			return ExitCode::FAILURE
 		}
-	}
+	};
+
+	let mut backup_storage = match TempBackupStorage::new(&svg_files) {
+		Ok(storage) => storage,
+		Err(e) => {
+			eprintln!("Error creating temporary backup storage: {e}");
+			if !quiet { println!("Your files weren't modified."); }
+			return ExitCode::FAILURE
+		}
+	};
+
+	backup_storage.disable_auto_cleanup();
 
 	if !no_default {
-		for file in &svg_files {
-			default_optimize(file, remove_fill)?;
+		if let Err(e) = default_optimize_files(&svg_files, remove_fill) {
+			eprintln!("Error optimizing files: {e}");
+			try_to_copy_back(&mut backup_storage, quiet);
+			return ExitCode::FAILURE
 		}
 	}
 
 	if use_svgo && svgo_path != None {
-		run_svgo(&svg_files, &svgo_path.unwrap())?;
+		if let Err(e) = run_svgo(&svg_files, &svgo_path.unwrap()) {
+			eprintln!("Error optimizing files with SVGO: {e}");
+			try_to_copy_back(&mut backup_storage, quiet);
+			return ExitCode::FAILURE
+		};
 	}
 
 	if compress_svgz {
-		for file in &svg_files {
-			compress_to_svgz(file)?;
+		if let Err(e) = compress_to_svgz(&svg_files) {
+			eprintln!("Error compressing files to .svgz format: {e}");
+			try_to_copy_back(&mut backup_storage, quiet);
+			return ExitCode::FAILURE
 		}
 	}
 
-	Ok(())
+	backup_storage.enable_auto_cleanup();
+
+	if !quiet {
+		println!("Files were successfully compressed.");
+	}
+
+	ExitCode::SUCCESS
+}
+
+fn try_to_copy_back(temp_storage: &mut TempBackupStorage, quiet: bool) {
+	if let Err(e) = temp_storage.copy_back() {
+		temp_storage.disable_auto_cleanup();
+		eprintln!("Error restoring files: {}.\nBackups are located in {} directory.", e, temp_storage.temp_dir().display());
+		return
+	} else {
+		temp_storage.enable_auto_cleanup();
+	}
+	if !quiet {
+		println!("Your files were restored.");
+	}
 }
